140c173e09cf8c7d48d81b4035ac76ff0f73c25a
cedric kofo <cedricsockeng@gmail.com>
2021-04-30 16:29:01 +0200

    Nouvelles corrections html, css et js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
+var supp = "";
+var tabInput = document.getElementById("form-section").getElementsByTagName("input");
 function appel(x){
-
     if(x.value == "Couple"){
+        supp="true";
         document.getElementById("insertHere").innerHTML='<div class="form-group form-input"><input type="text" name="prenom-cj" id="prenom-cj" required/><label for="prenom-cj" class="form-label">Prénom conjoint</label></div><div class="form-group form-input"><input type="text" name="nom-cj" id="name-cj" required/><label for="nom-cj" class="form-label">Nom du conjoint</label></div>';
     }
     var length = document.getElementById('insertHere').childNodes.length;
 
     if(x.value != "Couple" && length>0){
+        supp="false";
         document.getElementById("insertHere").innerHTML='';
     }
+
+    
 }
 
 function valider(){
@@ -18,7 +23,7 @@ function valider(){
     }else if(age<18){
         document.form.action="FormulaireB.html";
     }else if (age < 1) {
-        document.getElementById("reponse").innerText="L'age ne peut être négatif ou égale à 0"
+        alert("L'age ne peut être négatif ou égale à 0");
     }
     
 }
@@ -42,5 +47,48 @@ window.onclick = function(event) {
 
 function affichage(x){
     document.getElementById("dropbtn").value=x.value;
-    document.getElementById("dropbtn").innerHTML = document.getElementById("dropbtn").value + '<i class="fa fa-sort-down col"></i>';
+    document.getElementById("dropbtn").innerHTML = document.getElementById("dropbtn").value + '&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-sort-down col"></i>';
+    document.getElementById("hide").value=x.value;
+
 }
+
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;	
+	}
+	return vars;
+}
+
+function insert(){
+    tabInput = document.getElementById("form-section").getElementsByTagName("input");
+    for (let i = 0; i < tabInput.length; i++) {
+        tabInput[i].value = replaceAll("+"," ",decodeURI( $_GET( tabInput[i].name ) ));
+    }
+    
+    str = tabInput[3].value;
+    hex = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == "%") {
+            val1 = str.substring(0,i);
+            val2 = str.substring(i+3,str.length);
+        }
+    }
+   tabInput[3].value = val1+"@"+val2;
+    
+}
+
+function replaceAll(recherche, remplacement, chaineAModifier){
+    return chaineAModifier.split("+").join(" ");
+}
+
+document.getElementById("insertHere2").innerHTML='<div class="form-group form-input"><input type="text" name="prenom-cj" id="prenom-cj" required/><label for="prenom-cj" class="form-label">Prénom conjoint</label></div><div class="form-group form-input"><input type="text" name="nom-cj" id="name-cj" required/><label for="nom-cj" class="form-label">Nom du conjoint</label></div>';
+
+insert();
